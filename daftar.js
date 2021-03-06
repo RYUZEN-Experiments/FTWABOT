4de6f422b9318c5e4c64db635485dca7c6738287
ryuzenXperients <59796279+RYUZEN-Experiments@users.noreply.github.com>
2021-03-06 22:27:15 +0700

    Add files via upload

---
+++ daftar.js
@@ -0,0 +1,21 @@
+exports.menu = (id, Gabisa, Feedback) => {
+	return ${BotName}
+  
+HALLO , ${id.split("@s.whatsapp.net")[0]} 👋️ ini list pendaftaran
+
+*===========================*
+
+[-B] Sesi 1 
+
+[+S] Sesi 2
+
+*===========================*
+Note : pilih salah satu diatas, contoh +S anda telah memilih sesi ke 2.
+
+Kalo Masih Bingung Chat : 
+${+6281237677647}
+
+
+
+   [ POWERED BY ${BotName} ]`
+}
