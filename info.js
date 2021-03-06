080b4e46022352293cef4a599d4ed22ae8a56c7c
ryuzenXperients <59796279+RYUZEN-Experiments@users.noreply.github.com>
2021-03-06 23:47:37 +0700

    Add files via upload

---
+++ info.js
@@ -0,0 +1,19 @@
+exports.info = (id, Gabisa,Feedback ) => {
+	return ${BotName}
+  
+Hi, ${id.split("@s.whatsapp.net")[0]} 👋️
+
+ℹ️ INFO TOURNAMENT:
+   
+• *OWNER TOUR*: QULPAN
+• *IG PENANGGUNG JAWAB*: -
+• *AUTHOR BOT*: BANG RYUZENXX
+• *YOUTUBE*: MASLENT YT
+
+∆  INFO LAIN :
+
+    -
+
+
+             ${BotName}*             ]`
+}
